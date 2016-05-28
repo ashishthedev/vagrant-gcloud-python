3a30b1fa1ecd2ab15aeb5450fffc6ecfd449cfc4
Ashish Anand <ashishthedev@gmail.com>
2016-05-28 15:48:16 +0530

    Further refined

--- newapp/run.py
+++ newapp/run.py
@@ -4,16 +4,6 @@ __author__ = """ashishthedev@gmail.com (Ashish Anand)"""
 import newapp
 import config
 
-
-import logging
-from logging.handlers import RotatingFileHandler
-
-
 DEBUG = True
 app = newapp.create_app(config, debug=DEBUG)
-
-handler = RotatingFileHandler('newapp.log', maxBytes=100000, backupCount=1)
-handler.setLevel(logging.INFO)
-app.logger.addHandler(handler)
-
 
